b54cbc1e40810cc8ddc751e573131b844ca9d6d9
Jyle Vergara <junelynvergara@gmail.com>
2017-12-30 17:51:00 +0800

    add html css and js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//default time
+var timeControl = document.querySelector('input[type="time"]');
+timeControl.value = '00:00';
+
+
+//time checkbox custom to clear
+$('#customtime :checked').removeAttr('checked');
